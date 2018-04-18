a5be8ddff13767a1485ddab7224355c8e2ebc4f6
Werner Klaus <csam9224@student.uibk.ac.at>
2018-04-18 18:21:37 +0200

    BaseMap.at eingebunden

--- basemap.js
+++ basemap.js
@@ -1,7 +1,37 @@
 //console.log("Wallo Helt!");
 
 let myMap = L.map("mapdiv"); // L Leaflet Bibliothek, .map erzeugt neue Karte in mein mapdiv
-let myLayer = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"); //Hintergrundkarte mit Openstreet Map
 
-myMap.addLayer(myLayer); // Karte mit Hintergrundlayer verknüpfen
-myMap.setView([47.267,11.383],11); //Übergabe Koordinaten und Zoomfaktor für Karte
+//Hintergrundkarte mit Openstreet Map
+// {z} Zoom; {x} Länge {y} Breite {s} Subdomains, Kacheln
+let myLayers = {
+    osmlayer: L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"),
+    geolandbasemap: L.tileLayer("https://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png",{
+        subdomains : ["maps","maps1","maps2","maps3","maps4"],
+        attribution : "Datenquelle: <a href = 'https://www.basemap.at'>basemap.at</a>"
+        }
+    ),
+    bmapoverlay: L.tileLayer("https://{s}.wien.gv.at/basemap/bmapoverlay/normal/google3857/{z}/{y}/{x}.png",{
+        subdomains : ["maps","maps1","maps2","maps3","maps4"],
+        attribution : "Datenquelle: <a href = 'https://www.basemap.at'>basemap.at</a>"
+        }
+    ),
+    bmapgrau: L.tileLayer("https://{s}.wien.gv.at/basemap/bmapgrau/normal/google3857/{z}/{y}/{x}.png",{
+        subdomains : ["maps","maps1","maps2","maps3","maps4"],
+        attribution : "Datenquelle: <a href = 'https://www.basemap.at'>basemap.at</a>"
+        }
+    ),
+    bmaphidpi: L.tileLayer("https://{s}.wien.gv.at/basemap/bmaphidpi/normal/google3857/{z}/{y}/{x}.jpeg",{
+        subdomains : ["maps","maps1","maps2","maps3","maps4"],
+        attribution : "Datenquelle: <a href = 'https://www.basemap.at'>basemap.at</a>"
+        }
+    ),
+    bmaporthofoto30cm: L.tileLayer("https://{s}.wien.gv.at/basemap/bmaporthofoto30cm/normal/google3857/{z}/{y}/{x}.jpeg",{
+        subdomains : ["maps","maps1","maps2","maps3","maps4"],
+        attribution : "Datenquelle: <a href = 'https://www.basemap.at'>basemap.at</a>"
+        }
+    ),
+};
+
+myMap.addLayer(myLayers.geolandbasemap); // Karte mit Hintergrundlayer verknüpfen
+myMap.setView([47.267,11.383],11); //Übergabe Koordinaten (Array, Zentrum) und Zoomfaktor für Karte
