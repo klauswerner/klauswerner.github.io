7c9783ac52bc6f817b7056ef7fe01f77ea1146bd
Werner Klaus <csam9224@student.uibk.ac.at>
2018-04-28 12:44:47 +0200

    awstirol.js vorbereitet

--- awstirol.js
+++ awstirol.js
@@ -1,30 +1,13 @@
-let myMap = L.map("mapdiv"); // http://leafletjs.com/reference-1.3.0.html#map-l-map
+//1. Map erstellen und mit div verbinden
+let myMap = L.map("mapdiv"); 
 
-//Hintergrundkarte mit Openstreet Map
-// {z} Zoom; {x} Länge {y} Breite {s} Subdomains, Kacheln
+//Marker zu einer Gruppe zusammenfassen
+let markerAWS = L.featureGroup();
+
+//layers erstellen (BaseMap und Orthofoto)
 let myLayers = {
-    osmlayer: L.tileLayer(  //http://leafletjs.com/reference-1.3.0.html#tilelayer
-        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
-            subdomains : ["a","b","c"], //https://wiki.openstreetmap.org/wiki/Domain_names
-            attribution : "Datenquelle: <a href = 'https://www.openstreetmap.org'>openstreetmap.org</a>"
-            }),
     geolandbasemap: L.tileLayer("https://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png",{
-        subdomains : ["maps","maps1","maps2","maps3","maps4"], //http://leafletjs.com/reference-1.3.0.html#tilelayer-subdomains
-        attribution : "Datenquelle: <a href = 'https://www.basemap.at'>basemap.at</a>" //http://leafletjs.com/reference-1.3.0.html#layer-attribution
-        }
-    ),
-    bmapoverlay: L.tileLayer("https://{s}.wien.gv.at/basemap/bmapoverlay/normal/google3857/{z}/{y}/{x}.png",{
-        subdomains : ["maps","maps1","maps2","maps3","maps4"],
-        attribution : "Datenquelle: <a href = 'https://www.basemap.at'>basemap.at</a>"
-        }
-    ),
-    bmapgrau: L.tileLayer("https://{s}.wien.gv.at/basemap/bmapgrau/normal/google3857/{z}/{y}/{x}.png",{
-        subdomains : ["maps","maps1","maps2","maps3","maps4"],
-        attribution : "Datenquelle: <a href = 'https://www.basemap.at'>basemap.at</a>"
-        }
-    ),
-    bmaphidpi: L.tileLayer("https://{s}.wien.gv.at/basemap/bmaphidpi/normal/google3857/{z}/{y}/{x}.jpeg",{
-        subdomains : ["maps","maps1","maps2","maps3","maps4"],
+        subdomains : ["maps","maps1","maps2","maps3","maps4"], 
         attribution : "Datenquelle: <a href = 'https://www.basemap.at'>basemap.at</a>"
         }
     ),
@@ -35,28 +18,53 @@ let myLayers = {
     ),
 };
 
-myMap.addLayer(myLayers.geolandbasemap); // http://leafletjs.com/reference-1.3.0.html#map-addlayer Karte mit Hintergrundlayer verknüpfen
+//Einstellung Default Layer
+myMap.addLayer(myLayers.geolandbasemap); //Karte mit Hintergrundlayer verknüpfen
 
-let myMapControl = L.control.layers({   //http://leafletjs.com/reference-1.3.0.html#control-layers-l-control-layers
-    "OpenStreetMap" : myLayers.osmlayer,
+let myMapControl = L.control.layers({   
     "BaseMap.at" : myLayers.geolandbasemap,
-    "BaseMap Grau" : myLayers.bmapgrau,
-    "BaseMap High DPI" : myLayers.bmaphidpi,
     "BaseMap Orthofoto" : myLayers.bmaporthofoto30cm
 },{
-    "BaseMap Overlay" : myLayers.bmapoverlay,
+    "Marker" : markerAWS,
 },{
-    collapsed: false             //http://leafletjs.com/reference-1.3.0.html#control-layers-collapsed
+    collapsed: true
 });
-myMap.addControl(myMapControl); //http://leafletjs.com/reference-1.3.0.html#map-addcontrol
+myMap.addControl(myMapControl);
 
-myMap.setView([47.267,11.383],11); //http://leafletjs.com/reference-1.3.0.html#map-setview Übergabe Koordinaten (Array, Zentrum) und Zoomfaktor für Karte
+//myMap.setView([47.267,11.383],11); //Übergabe Koordinaten (Array, Zentrum) und Zoomfaktor für Karte
 
-//http://leafletjs.com/reference-1.3.0.html#control-scale-l-control-scale
 let myMapScale = L.control.scale(
-    {position: "bottomleft",   // Default bereits bottomleft; http://leafletjs.com/reference-1.3.0.html#control-scale-position
-    metric: true,            //http://leafletjs.com/reference-1.3.0.html#control-scale-metric
-    imperial: false,          // http://leafletjs.com/reference-1.3.0.html#control-scale-imperial
-    maxWidth: 200}             //http://leafletjs.com/reference-1.3.0.html#control-scale-maxwidth
+    {position: "bottomleft",
+    metric: true,
+    imperial: false,
+    maxWidth: 200}
 ).addTo(myMap);
-//myMap.addControl.scale(myMapScale);
+
+//Koordinaten
+const Koordinaten = {
+    uni : [47.264, 11.385],
+    usi : [47.257, 11.356],
+    technik: [47.263, 11.343],
+    igls : [47.230, 11.408],
+    patscherkofl : [47.208, 11.460]
+};
+
+//Marker
+const markerOptions = {
+    title: "Universität Innsbruck",     //http://leafletjs.com/reference-1.3.0.html#marker-title
+    draggable: true,                    //http://leafletjs.com/reference-1.3.0.html#marker-draggable
+    opacity: 0.80                       //http://leafletjs.com/reference-1.3.0.html#marker-opacity
+}
+
+L.marker(Koordinaten.uni, markerOptions).addTo(markerGroup);    //http://leafletjs.com/reference-1.3.0.html#marker
+L.marker(Koordinaten.usi, markerOptions).addTo(markerGroup);
+L.marker(Koordinaten.technik, markerOptions).addTo(markerGroup);
+
+//PopUps
+let patscherkoflmarker = L.marker(Koordinaten.patscherkofl, markerOptions2).addTo(markerGroup2); 
+L.marker(Koordinaten.igls, markerOptions2).addTo(markerGroup2); 
+
+patscherkoflmarker.bindPopup("<p>Patscherkofl:</p><img style='width:200px'src='https://apps.tirol.gv.at/luft/nordkette.jpg' alt='Patscherkofl' />");
+
+//Start Zoom
+myMap.fitBounds(markerAWS.getBounds()); 
