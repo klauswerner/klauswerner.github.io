e42668dbe5a9af4f6fd42f22710ae55158ba9338
Werner Klaus <csam9224@student.uibk.ac.at>
2018-05-09 17:23:59 +0200

    Leaflet Search eingebunden

--- wien_citybike.js
+++ wien_citybike.js
@@ -38,21 +38,27 @@ let myLayers = {
     ),
 };
 
+//Default Hintergrundkarte
 myMap.addLayer(myLayers.geolandbasemap); 
 
-let myMapControl = L.control.layers({
+//Default Overlaykarte
+myMap.addLayer(wienGroup_cb);
 
+//Kartenansichten schalten und zu Karte hinzufügen(aktivieren)
+let myMapControl = L.control.layers({
     "BaseMap.at" : myLayers.geolandbasemap,
     "BaseMap Orthofoto" : myLayers.bmaporthofoto30cm
 },{
     "CityBikes" : wienGroup_cb,
     "CityBikes Cluster" : clmarkers
-
 },{ 
     collapsed: true
-});
-myMap.addControl(myMapControl);
+}).addTo(myMap);
 
+//Kartenansichten Kontrollpanel aktivieren
+//myMap.addControl(myMapControl);
+
+//Maßstaß erstellen und hinzufügen
 let myMapScale = L.control.scale(
     {position: "bottomleft",
     metric: true,
@@ -60,9 +66,9 @@ let myMapScale = L.control.scale(
     maxWidth: 200}
 ).addTo(myMap);
 
-//myMap.addLayer(wienGroup_cb);
-
 //Daten vom Server über URL holen und Laden
+const url = "https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:CITYBIKEOGD&srsName=EPSG:4326&outputFormat=json"
+
 async function addGeoJson(url){
     //console.log("URL wird geladen: ...", url);
     const response = await fetch(url);
@@ -72,7 +78,7 @@ async function addGeoJson(url){
     const geojson = L.geoJSON(wiendata,{
 style: function(feature){
     return {color: "#ff0000"}; 
-},
+},  //Icons definieren
     pointToLayer: function(geoJsonPoint, latlng){
     return L.marker(latlng, {
         icon: L.icon({
@@ -81,7 +87,9 @@ style: function(feature){
     });
  }
 });
+    //Einzelne Punkte aus der GEOJson zur MarkerGroup hinzufügen
     wienGroup_cb.addLayer(geojson);
+    //Popups erstellen
     geojson.bindPopup(function(layer){
         const props = layer.feature.properties;
         const popupText = `<h1>${props.STATION}</h1>
@@ -89,19 +97,26 @@ style: function(feature){
         return popupText;
     
     });
+    //Default Zoom Ansicht auf MarkerGroup
     myMap.fitBounds(wienGroup_cb.getBounds()); 
 
+    //Leaflet Hash Plugin
     const hash = new L.Hash(myMap);
 
-
+    //Leaflet MarkerCluster Plugin
     clmarkers.addLayer(wienGroup_cb);
-    myMap.addLayer(clmarkers);
-}
+    //myMap.addLayer(clmarkers);
 
-const url = "https://data.wien.gv.at/daten/geo?service=WFS&request=GetFeature&version=1.1.0&typeName=ogdwien:CITYBIKEOGD&srsName=EPSG:4326&outputFormat=json"
+    //Leaflet Search als Control hinzufügen
+    myMap.addControl(new L.Control.Search({
+        layer: wienGroup_cb,
+        propertyName: "STATION"
+        })
+    );
+    
+}
 
 //Aufruf der Funktion
 addGeoJson(url);
 
-//Leaflet Hash Plugin
-//Leaflet Marker Cluster Plugin
+//Leaflet search
